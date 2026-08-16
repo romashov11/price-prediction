052e9fcac826fed1b8e0efd539cfb37b911d37a0
romashov11 <mnopdaser@gmail.com>
2026-08-16 20:18:53 +0300

    add error listener

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,13 @@
 function get_params(){
     const btn = document.getElementById("count_result")
+/*    document.addEventListener('keydown', function(event) {
+        // Ваш код здесь
+        console.log('Нажата клавиша:', event.key);
+        });
+*/
+    const err = document.getElementById('er')
+    err.classList.add("hid")
+
     btn.addEventListener('click', () => {
         
         const city = document.getElementById('city').value
@@ -7,19 +15,30 @@ function get_params(){
         const rooms = document.getElementById('rooms').value
         const floors = document.getElementById('floors').value
         const metro = document.getElementById('metres_underground').value
+        
 
-        const data = {
-        city: city,
-        metres: parseFloat(metres) || 0,      // Преобразуем в число
-        rooms: parseInt(rooms) || 0,           // Преобразуем в целое число
-        floors: parseInt(floors) || 1,         // Преобразуем в целое число
-        metro: parseFloat(metro) || 0
-    }
+        if (metres <= 0 || floors <= 0 || metro <= 0) {
+            err.classList.add("unhid")
+            document.getElementById('metres').value = 0
+            document.getElementById('floors').value = 0
+            document.getElementById('metres_underground').value = 0
+            
+            
 
-    
-        alert(data.rooms)
+        }
 
-        console.log(data)
+        else{
+            err.classList.remove("unhid")
+            const data = {
+            city: city,
+            metres: parseFloat(metres) || 0,      // Преобразуем в число
+            rooms: parseInt(rooms) || 0,           // Преобразуем в целое число
+            floors: parseInt(floors) || 1,         // Преобразуем в целое число
+            metro: parseFloat(metro) || 0
+        }
+        }
+
+        
     })
     
 }
